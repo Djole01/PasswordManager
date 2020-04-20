f348e6a06ed4c3a02d1fdc52462d60b4f07ae388
Djole01 <djordje_obradovic33@mm.st>
2020-04-20 16:54:53 +0300

    separated program into multiple files

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : writeToFIle.cpp
+// Author      : Viope C++ course
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Writing to a file in C++
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <fstream>
+#include "ReadFile.h"
+using namespace std;
+
+int main() {
+
+	readFromFIle();
+
+	return 0;
+}

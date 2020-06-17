09b1052600672f3615a15622afcf06a20e0536e5
Djole01 <djordje_obradovic33@mm.st>
2020-06-17 11:30:36 +0300

    master password work in progress

--- MasterPassword.cpp
+++ MasterPassword.cpp
@@ -14,39 +14,37 @@
 
 using namespace std;
 
+int masterPasswordIsSet();
 
+int flag;
 void readFromFIleMaster() {
 
-	// open MasterPass.txt for reading
-	ifstream read("/home/sdfe/1_Personal_Work/cpp_workspace/PasswordManager/src/MasterPass.txt");
+	flag = masterPasswordIsSet();
+	cout << flag << endl;
+	if (flag == 0){	// password isnt set, create one
 
-	// if reading is successful
-	if (read.is_open()) {
-
-		// read first line to see if master password has been set already.
-
-		string isSet1;
-		string masterPasswordIsSet;
-		int line_no = 0;
-		while (line_no != 1 && getline(read, isSet1)) {
-		    ++line_no;
+		string masterPassword;
+		string masterPassword2;
+		cout << "Master password not set.\nEnter new Master password:\n";
+		cin >> masterPassword;
+		cout << "Confirm it by entering it again:\n";
+		cin >> masterPassword2;
+		if (masterPassword == masterPassword2){
+			cout << "Password set";
+			// write file 1
+			// writeMasterPass();
 		}
-		if (line_no == 1) {
-			read >> isSet1;
-		}
-		else {
-			cout << "Error with master password file" << endl;
-		}
-
-		cout << isSet1 << endl;
-	//	while (read >> masterPasswordIsSet ) {
-	//		IsSet2 = masterPasswordIsSet;
-	//	}
-
-	} else {
-		cout << "Unable to open the file!" << endl;
-		exit(-1);
 	}
+	else {	// password is set, log in.
+
+		string tempMP;
+		cout << "enter master password:\n";
+		cin >> tempMP;
+		// open the file and see if tempMp matches line 2.
+
+	}
+
+
 }
 
 
@@ -55,4 +53,34 @@ void readFromFIleMaster() {
 // if it is, ask user to enter that password, check if it matches.
 // make this all a loop.
 
+int masterPasswordIsSet(){
 
+	// open MasterPass.txt for reading
+		ifstream read("/home/sdfe/1_Personal_Work/cpp_workspace/PasswordManager/src/MasterPass.txt");
+
+		// if reading is successful
+		if (read.is_open()) {
+
+			// read first line to see if master password has been set already.
+
+			string isSet1;
+			string masterPasswordIsSet;
+			int line_no = 0;
+			while (line_no != 1 && getline(read, isSet1)) {
+			    ++line_no;
+			}
+			if (line_no == 1) {
+				read >> isSet1;
+			}
+			else {
+				cout << "Error with master password file" << endl;
+			}
+
+			flag = std::stoi( isSet1 );
+			return flag;
+
+		} else {
+			cout << "Unable to open the file!" << endl;
+			exit(-1);
+		}
+}

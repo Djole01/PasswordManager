bcb6732a25eea5d71360f4e82f9bb0b8e63fad30
Djordje <djordje_obradovic33@protonmail.com>
2021-09-11 20:30:33 +0300

    including encryption while working on it

---
+++ include/myHiddenCharManipulaion.h
@@ -0,0 +1,11 @@
+#ifndef MYHIDDENCHARMANIPULAION_H_
+#define MYHIDDENCHARMANIPULAION_H_
+
+
+#define HIDDENROUND1 while(fps>>noskipws>>ch){ch = ch + 2; fpt<<ch;}
+#define HIDDENROUND2 while(fps>>noskipws>>ch){ch = ch + 3; fpt<<ch;}
+
+#define HIDDENDECRYPT1 while(fpt>>noskipws>>ch){ch = ch - 2; fps<<ch;}
+#define HIDDENDECRYPT2 while(fpt>>noskipws>>ch){ch = ch - 3; fps<<ch;}
+
+#endif // MYHIDDENCHARMANIPULAION_H_
